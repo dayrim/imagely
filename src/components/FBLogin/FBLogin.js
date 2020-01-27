f9d687d12d3ee5e0b453a91091b3b6fd2503dc83
THIRDEYE\Dmitrijs <dmitrijs.pavlovs123@gmail.com>
2020-01-27 06:18:32 +0200

    Added translation for login button

--- src/components/FBLogin/FBLogin.js
+++ src/components/FBLogin/FBLogin.js
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { Facebook } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+import { useTranslation } from "react-i18next";
+
 const useStyles = makeStyles(theme => ({
   root: {
     height: "50px",
@@ -16,6 +18,7 @@ const useStyles = makeStyles(theme => ({
 
 const FBLogin = ({ onClick }) => {
   const classes = useStyles();
+  const { t } = useTranslation("translation");
   return (
     <>
       <Button
@@ -26,7 +29,7 @@ const FBLogin = ({ onClick }) => {
         onClick={onClick}
         className={classes.root}
       >
-        LOG IN WITH FACEBOOK
+        {t("login")}
       </Button>
     </>
   );
